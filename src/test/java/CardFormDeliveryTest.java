1b06285b039085c365fe7c22f941f29f86e8d977
alekseev <alekseev.m@me.com>
2022-10-26 22:18:30 +0300

    update8

--- src/test/java/CardFormDeliveryTest.java
+++ src/test/java/CardFormDeliveryTest.java
@@ -1,6 +1,10 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -12,28 +16,41 @@ import static com.codeborne.selenide.Condition.*;
 
 public class CardFormDeliveryTest {
 
-
-        String generateDate(int days, String pattern) {
-            return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
-        }
-
-        @BeforeEach
-        void openUrlInBrowser() {
-            open("http://localhost:9999");
-        }
-
-        @Test
-        void shouldSuccessfulSentForm() {
-            String date = generateDate(3, "dd.MM.yyyy");
-            $("[data-test-id='city'] input").setValue("Пермь");
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-            $("[data-test-id='date'] input").setValue(date);
-            $("[data-test-id='name'] input").setValue("Михаил Алексеев");
-            $("[data-test-id='phone'] input").setValue("+79991234567");
-            $("[data-test-id='agreement']").click();
-            $$("button").find(exactText("Забронировать")).click();
-            $("[data-test-id='notification']  .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(exactText("Успешно!"));
-            $("[data-test-id='notification']  .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно забронирована на " + date));
-        }
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
     }
+
+
+    @BeforeEach
+    String generateDate(int days, String pattern) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    void openUrlInBrowser() {
+        open("http://localhost:9999");
+    }
+
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        ChromeDriver driver = new ChromeDriver(options);
+    }
+
+    @Test
+    void shouldSuccessfulSentForm() {
+        String date = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='city'] input").setValue("Пермь");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(date);
+        $("[data-test-id='name'] input").setValue("Михаил Алексеев");
+        $("[data-test-id='phone'] input").setValue("+79991234567");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']  .notification__title").shouldBe(visible, Duration.ofSeconds(15)).shouldBe(exactText("Успешно!"));
+        $("[data-test-id='notification']  .notification__content").shouldBe(visible, Duration.ofSeconds(15)).shouldHave(text("Встреча успешно забронирована на " + date));
+    }
+}
 
